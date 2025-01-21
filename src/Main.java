17f5dc6bf76132437bb0f8a85a66cf0780d9c895
insai <insaittlg@gmail.com>
2025-01-21 22:27:34 +0300

    Выполненая работа методов.

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,7 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        int clientOS = 0;
-
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке. ");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке. ");
-        }
-        System.out.println();
-
-
-        int clientDeviceYear = 2012;
-        int clientOS1 = 0;
-        if (clientOS1 == 0 & clientDeviceYear > 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке. ");
-        } else if (clientOS1 == 0 & clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для IOS по ссылке. ");
-        } else if (clientOS1 == 1 & clientDeviceYear > 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке. ");
-        } else {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке. ");
-        }
-        System.out.println();
-
-
-        int year = 2549;
+    public static void searchBissextileYear(int year) {
         if (year <= 1584) {
             System.out.println("Год должен быть больше 1584");
         } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
@@ -33,9 +10,24 @@ public class Main {
             System.out.println(year + " не является высокосным годом.");
         }
         System.out.println();
+    }
 
+    public static void versionDetection(int currentYear, int clientOS1) {
 
-        int deliveryDistance = 95;
+        if (clientOS1 == 0 & currentYear > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке. ");
+        } else if (clientOS1 == 0 & currentYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке. ");
+        } else if (clientOS1 == 1 & currentYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке. ");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке. ");
+        }
+        System.out.println();
+    }
+
+    public static void bankCardDelivery(int deliveryDistance) {
+
         int deliveryTime;
         if (deliveryDistance <= 20) {
             deliveryTime = 1;
@@ -49,49 +41,21 @@ public class Main {
         } else {
             System.out.println("Свыше 100 км доставки нет. ");
         }
-        System.out.println();
+    }
 
+    public static void main(String[] args) {
 
-        int monthNumber = 12;
-        switch (monthNumber) {
-            case 1:
-                System.out.println(" 1-й месяц Январь принадлежит к сезону Зима ");
-                break;
-            case 2:
-                System.out.println(" 1-й месяц Февраль принадлежит к сезону Зима ");
-                break;
-            case 3:
-                System.out.println(" 1-й месяц Март принадлежит к сезону Весна ");
-                break;
-            case 4:
-                System.out.println(" 1-й месяц Апрель принадлежит к сезону Весна ");
-                break;
-            case 5:
-                System.out.println(" 1-й месяц Май принадлежит к сезону Весна ");
-                break;
-            case 6:
-                System.out.println(" 1-й месяц Июнь принадлежит к сезону Лето ");
-                break;
-            case 7:
-                System.out.println(" 1-й месяц Июль принадлежит к сезону Лето ");
-                break;
-            case 8:
-                System.out.println(" 1-й месяц Август принадлежит к сезону Лето ");
-                break;
-            case 9:
-                System.out.println(" 1-й месяц Сентябрь принадлежит к сезону Осень ");
-                break;
-            case 10:
-                System.out.println(" 1-й месяц Октябрь принадлежит к сезону Осень ");
-                break;
-            case 11:
-                System.out.println(" 11-й месяц Ноябрь принадлежит к сезону Осень ");
-                break;
-            case 12:
-                System.out.println(" 12-й месяц Декабрь принадлежит к сезону Зима ");
-                break;
-            default:
-                System.out.println(" Такого месяца не существует. ");
-        }
+        System.out.println("Задание №1");
+        int year = 2549;
+        searchBissextileYear(year);
+
+        System.out.println("Задание №2");
+        int currentYear = LocalDate.now().getYear();
+        int clientOS1 = 0;
+        versionDetection(currentYear, clientOS1);
+
+        System.out.println("Задание №3");
+        int deliveryDistance = 95;
+        bankCardDelivery(deliveryDistance);
     }
 }
